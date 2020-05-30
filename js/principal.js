8b9d7d706f9d0245dd59891bae6ee722d7c50cd6
Higor Nóbrega <higorst.nobrega@gmail.com>
2020-05-30 19:18:06 -0300

    Colocando o formulario na tabela

--- js/principal.js
+++ js/principal.js
@@ -79,10 +79,32 @@ for (var i = 0; i < pacientes.length; i++) {
 }
 adicionarPaciente.addEventListener("click",function(event){
     event.preventDefault()
-    console.log("Clicou")
-});
-titulo.addEventListener("click", mostraMensagem);
+    var formulario = document.querySelector("#form-adiciona")
+    var nome = formulario.nome.value;
+    var altura = formulario.altura.value;
+    var peso = formulario.peso.value;
+    var gordura = formulario.gordura.value;
 
-function mostraMensagem() {
-    console.log("Olá eu fui clicado!");
-}
+    var pacienteTr = document.createElement("tr");
+
+    var nomeTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    nomeTd.textContent = nome;
+    alturaTd.textContent = altura;
+    pesoTd.textContent = peso;
+    gorduraTd.textContent = gordura;
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(gorduraTd);
+    pacienteTr.appendChild(imcTd);
+
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
+
+});

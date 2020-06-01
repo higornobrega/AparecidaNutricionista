a05eb3d0afb01591ff7cd217beb8732a7ae9a423
Higor Nóbrega <higorst.nobrega@gmail.com>
2020-06-01 13:09:39 -0300

    Validando o restante dos campos

--- js/forms.js
+++ js/forms.js
@@ -4,16 +4,19 @@ adicionarPaciente.addEventListener("click", function (event) {
     var formulario = document.querySelector("#form-adiciona")
     var paciente = obtemPacienteFormulario(formulario);
 
+    var pacienteTr = montaTr(paciente);
     var erros = validaPaciente(paciente);
-    var pacienteTr = montaTr(paciente)
-    if (erro.length > 0) {
-        var err = document.querySelector("#mensagem-erro");
-        err.textContent = erros;
+
+    if (erros.length > 0) {
+        exibeMensagensDeErro(erros)
         return;
     }
     var tabela = document.querySelector("#tabela-pacientes");
     tabela.appendChild(pacienteTr);
+    var ul = document.querySelector("#mensagem-erro");
+    ul.innerHTML = "";
     formulario.reset();
+    
 });
 
 function obtemPacienteFormulario(formulario) {
@@ -49,6 +52,9 @@ function criaTd(dado, classe) {
 
 function validaPaciente(paciente) {
     var erros = [];
+    if (paciente.nome.length == 0) {
+        erros.push("Nome não pode ser em branco");
+    }
     if (!validaPeso(paciente.peso)) {
         erros.push("Peso inválido");
     }
@@ -56,5 +62,21 @@ function validaPaciente(paciente) {
     if (!validaAltura(paciente.altura)) {
         erros.push("Altura inválida");
     }
+    if (paciente.gordura.length == 0) {
+        erros.push("A gordura não pode ser em branco");
+    }
+    if (paciente.peso.length == 0) {
+        erros.push("O peso não pode ser em branco");
+    }
     return erros;
+}
+
+function exibeMensagensDeErro(erros) {
+    var ul = document.querySelector("#mensagem-erro");
+    ul.innerHTML = "";
+    erros.forEach(function(erro){
+        var li = document.createElement("li")
+        li.textContent = erro;
+        ul.appendChild(li)
+    });
 }

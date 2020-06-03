ad2bc2160ba39a937cdd8babd5cadcce38055db8
Higor Nóbrega <higorst.nobrega@gmail.com>
2020-06-03 18:19:11 -0300

    Removendo com efeito

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -3,9 +3,13 @@ console.log(pacientes);
 
 var tabela = document.querySelector("table");
 tabela.addEventListener("dblclick", function(event){//event quem foi clicado
-    var alvoEvento = event.target; //Onde foi clicado
-    var paiDoAlvo = alvoEvento.parentNode; //Pai de quem foi clicado
-    paiDoAlvo.remove();
+    event.target.parentNode.classList.add("fadeOut");
+    setTimeout(function(){
+        var alvoEvento = event.target; //Onde foi clicado
+        var paiDoAlvo = alvoEvento.parentNode; //Pai de quem foi clicado
+        paiDoAlvo.remove();
+    }, 500);
+    
 });
 
 // pacientes.forEach(function(paciente){
